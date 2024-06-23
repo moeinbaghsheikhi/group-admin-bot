fd87d2b5535895d90d33f2b68d902ff93a239109
moein <moein.baghsheikhi@gmail.com>
2024-06-23 16:55:31 +0330

    invited system

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const bot = new Telegraf("7281236571:AAEwm5RnmL5b6G6N9qW00zQwW5aPQgZnPkk")
 
 // bot.start((ctx) => ctx.reply('Welcome'))
 
-bot.on('new_chat_members', (ctx) => {
+bot.on('new_chat_members', async (ctx) => {
     ctx.reply(`${ctx.message.new_chat_member.first_name} عزیز. \n به گروه ما خوش اومدی!`)
+
+    // check invite
+    const newMembers = ctx.message.new_chat_members
+    const inviterId = ctx.message.from.id
+
+    for(const member of newMembers) {
+        await client.incr(`invited:${inviterId}`)
+    }
 });
 
 bot.on('left_chat_member', (ctx) => {
@@ -27,60 +35,68 @@ bot.on("text", async (ctx) => {
 
     const untilDate = Math.floor(Date.now() / 1000) + 1800
 
-    if(ctx.message.reply_to_message){
-        // get role 
-        const chatMemberRole = await ctx.telegram.getChatMember(groupId, chatId)
-
-        if(message.startsWith('/')){
-            if(chatMemberRole.status == "creator" || chatMemberRole.status == "adminstrator"){
-                const userId = ctx.message.reply_to_message.from.id
-                if(message == "/ban") {
+    const invitedCount = await client.get(`invited:${chatId}`)
+    console.log(invitedCount)
+    if(invitedCount > 10) {
+        if(ctx.message.reply_to_message){
+            // get role 
+            const chatMemberRole = await ctx.telegram.getChatMember(groupId, chatId)
+    
+            if(message.startsWith('/')){
+                if(chatMemberRole.status == "creator" || chatMemberRole.status == "adminstrator"){
+                    const userId = ctx.message.reply_to_message.from.id
+                    if(message == "/ban") {
+                            ctx.restrictChatMember(userId, {
+                            until_date: untilDate,
+                            can_send_messages: false
+                        })
+                        ctx.reply(`کاربر ${ctx.message.reply_to_message.from.first_name} با موفقیت بن شد`)
+                    }
+                    else if(message == "/unban"){
                         ctx.restrictChatMember(userId, {
-                        until_date: untilDate,
-                        can_send_messages: false
-                    })
-                    ctx.reply(`کاربر ${ctx.message.reply_to_message.from.first_name} با موفقیت بن شد`)
+                            can_send_messages: true
+                        })
+                        ctx.reply(`کاربر ${ctx.message.reply_to_message.from.first_name} با موفقیت آزاد شد`)
+                    }else if(message == "/promote"){
+                        ctx.promoteChatMember(userId, {
+                            can_promote_members: true,
+                            can_pin_messages: true,
+                            can_change_info: true,
+                            can_send_polls: true
+                        })
+                    } else if(message == "/demote"){
+                        ctx.restrictChatMember(userId,{
+                            permissions: {
+                                can_promote_members: false,
+                                can_pin_messages: false,
+                                can_change_info: false,
+                                can_send_polls: false
+                            }
+                        })
+                    }
+                } else {
+                    ctx.reply("شما دسترسی این کارو نداری!")
                 }
-                else if(message == "/unban"){
-                    ctx.restrictChatMember(userId, {
-                        can_send_messages: true
-                    })
-                    ctx.reply(`کاربر ${ctx.message.reply_to_message.from.first_name} با موفقیت آزاد شد`)
-                }else if(message == "/promote"){
-                    ctx.promoteChatMember(userId, {
-                        can_promote_members: true,
-                        can_pin_messages: true,
-                        can_change_info: true,
-                        can_send_polls: true
-                    })
-                } else if(message == "/demote"){
-                    ctx.restrictChatMember(userId,{
-                        permissions: {
-                            can_promote_members: false,
-                            can_pin_messages: false,
-                            can_change_info: false,
-                            can_send_polls: false
-                        }
-                    })
-                }
-            } else {
-                ctx.reply("شما دسترسی این کارو نداری!")
             }
         }
+    
+        if(forbiddenWords.some(word => message.includes(word))){
+            // set and check forbidden keys
+            await client.incr(`user:${chatId}:forbiddenWord`)
+            const countForbidden = await client.get(`user:${chatId}:forbiddenWord`)
+    
+            if(countForbidden >= 500) {
+                ctx.banChatMember(chatId)
+            } else {
+                ctx.deleteMessage();
+                ctx.reply("پیام شما حاوی کلمات نامناسب بود و حذف شد!")
+            }
+        } 
+    }else {
+        ctx.deleteMessage()
+        ctx.reply(`کاربر عزیز ${ctx.message.from.first_name} \n شما برای استفاده از این گروه و ارسال پیام باید ابتدا 10 نفر رو به گروه اضافه کنی! \n تعداد نفراتی که تا الان دعوت کردی: ${invitedCount} \n تعداد نفراتی که باید اضافه کنی: ${(10 - invitedCount)}`)
     }
 
-    if(forbiddenWords.some(word => message.includes(word))){
-        // set and check forbidden keys
-        await client.incr(`user:${chatId}:forbiddenWord`)
-        const countForbidden = await client.get(`user:${chatId}:forbiddenWord`)
-
-        if(countForbidden >= 500) {
-            ctx.banChatMember(chatId)
-        } else {
-            ctx.deleteMessage();
-            ctx.reply("پیام شما حاوی کلمات نامناسب بود و حذف شد!")
-        }
-    } 
 }) 
 
 bot.launch()
